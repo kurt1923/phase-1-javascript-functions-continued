003f972ace298c1e157b14f451b0523d8e770493
Kurt Vermillion <kurtv0727@gmail.com>
2022-06-29 20:25:05 -0400

    commit tree 4d6bb8476e980f92855ac6e8f09f762ae79817c8

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+ function saturdayFun(sex="roller-skate"){ 
+    return `This Saturday, I want to ${sex}!`
+}
+function mondayWork(message='go to the office'){
+    return `This Monday, I will ${message}.`
+}
+function wrapAdjective(message="*"){
+    return function (adj='special'){
+        return `You are ${message}${adj}${message}!`}
+    }
+
+
+
